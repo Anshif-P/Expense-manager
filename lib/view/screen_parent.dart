116088527ad7bbbc6fb6fe1c19ce5412f0117556
Anshif-P <129931514+Anshif-P@users.noreply.github.com>
2024-02-24 12:14:25 +0530

    updation

--- lib/view/screen_parent.dart
+++ lib/view/screen_parent.dart
@@ -35,24 +35,6 @@ class _ScreenParentState extends State<ScreenParent> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      // floatingActionButton: CircleAvatar(
-      //   radius: 31,
-      //   backgroundColor: CustomColors.bottomAddButtomGeyColor,
-      //   child: Transform.scale(
-      //     scale: .9,
-      //     child: FloatingActionButton(
-      //       elevation: 0,
-      //       backgroundColor: CustomColors.greyBackgroundColor,
-      //       onPressed: () async {},
-      //       child: const Icon(
-      //         Icons.add,
-      //         color: Colors.white,
-      //         size: 30,
-      //       ),
-      //     ),
-      //   ),
-      // ),
       body: PageView(
         controller: _pageController,
         physics: const NeverScrollableScrollPhysics(),
@@ -85,17 +67,6 @@ class _ScreenParentState extends State<ScreenParent> {
                   ),
                   itemLabel: 'Page 1',
                 ),
-                // BottomBarItem(
-                //   inActiveItem: Icon(
-                //     Icons.add,
-                //     color: Colors.white,
-                //   ),
-                //   activeItem: Icon(
-                //     Icons.add,
-                //     color: Colors.blueAccent,
-                //   ),
-                //   itemLabel: 'Page 2',
-                // ),
                 BottomBarItem(
                   inActiveItem: Icon(
                     Icons.history,
@@ -107,28 +78,6 @@ class _ScreenParentState extends State<ScreenParent> {
                   ),
                   itemLabel: 'Page 2',
                 ),
-                // BottomBarItem(
-                //   inActiveItem: Icon(
-                //     Icons.today_rounded,
-                //     color: Colors.white,
-                //   ),
-                //   activeItem: Icon(
-                //     Icons.today_rounded,
-                //     color: Colors.blueAccent,
-                //   ),
-                //   itemLabel: 'Page 2',
-                // ),
-                // BottomBarItem(
-                //   inActiveItem: SvgPicture.asset(
-                //     'lib/asset/svg/dashboard.svg',
-                //     color: Colors.blueGrey,
-                //   ),
-                //   activeItem: SvgPicture.asset(
-                //     'lib/asset/svg/dashboard.svg',
-                //     color: Colors.white,
-                //   ),
-                //   itemLabel: 'Page 3',
-                // ),
               ],
               onTap: (index) {
                 _pageController.jumpToPage(index);
@@ -138,109 +87,3 @@ class _ScreenParentState extends State<ScreenParent> {
     );
   }
 }
-
-
-
-
-//////--------------------------------------------------------------------------------  basic one -----------------//
-
-// class _ScreenParentState extends State<ScreenParent> {
-//   /// Controller to handle PageView and also handles initial page
-//   final _pageController = PageController(initialPage: 0);
-
-//   /// Controller to handle bottom nav bar and also handles initial page
-//   final _controller = NotchBottomBarController(index: 0);
-
-//   int maxCount = 5;
-
-//   @override
-//   void dispose() {
-//     _pageController.dispose();
-//     super.dispose();
-//   }
-
-//   /// widget list
-//   final List<Widget> bottomBarPages = [
-//     const ScreenHome(),
-//     const ScreenAdd(),
-//     const ScreenHistory(),
-//   ];
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: PageView(
-//         controller: _pageController,
-//         physics: const NeverScrollableScrollPhysics(),
-//         children: List.generate(
-//             bottomBarPages.length, (index) => bottomBarPages[index]),
-//       ),
-//       extendBody: true,
-//       bottomNavigationBar: (bottomBarPages.length <= maxCount)
-//           ? AnimatedNotchBottomBar(
-//               /// Provide NotchBottomBarController
-//               notchBottomBarController: _controller,
-//               color: CustomColors.greyBackgroundColor,
-//               showLabel: false,
-//               // shadowElevation: 5,
-//               // kBottomRadius: 28.0,
-//               // notchShader: const SweepGradient(
-//               //   startAngle: 0,
-//               //   endAngle: pi / 2,
-//               //   colors: [Colors.red, Colors.green, Colors.orange],
-//               //   tileMode: TileMode.mirror,
-//               // ).createShader(Rect.fromCircle(center: Offset.zero, radius: 8.0)),
-//               notchColor: Colors.black87,
-
-//               /// restart app if you change removeMargins
-//               removeMargins: false,
-//               bottomBarWidth: 500,
-//               durationInMilliSeconds: 300,
-//               bottomBarItems: [
-//                 const BottomBarItem(
-//                   inActiveItem: Icon(
-//                     Icons.home_filled,
-//                     color: Colors.blueGrey,
-//                   ),
-//                   activeItem: Icon(
-//                     Icons.home_filled,
-//                     color: Colors.blueAccent,
-//                   ),
-//                   itemLabel: 'Page 1',
-//                 ),
-//                 const BottomBarItem(
-//                   inActiveItem: Icon(
-//                     Icons.star,
-//                     color: Colors.blueGrey,
-//                   ),
-//                   activeItem: Icon(
-//                     Icons.star,
-//                     color: Colors.blueAccent,
-//                   ),
-//                   itemLabel: 'Page 2',
-//                 ),
-
-//                 ///svg example
-//                 BottomBarItem(
-//                   inActiveItem: SvgPicture.asset(
-//                     'lib/asset/svg/dashboard.svg',
-//                     color: Colors.blueGrey,
-//                   ),
-//                   activeItem: SvgPicture.asset(
-//                     'lib/asset/svg/dashboard.svg',
-//                     color: Colors.white,
-//                   ),
-//                   itemLabel: 'Page 3',
-//                 ),
-//               ],
-//               onTap: (index) {
-//                 /// perform action on tab change and to update pages you can update pages without pages
-//                 // log('current selected index $index');
-//                 _pageController.jumpToPage(index);
-//               },
-//               // kIconSize: 24.0,
-//             )
-//           : null,
-//     );
-//   }
-// }
